52c2297a2c4f6b662439d175e439ff2fe4a0bd0f
Lucas <lucasezequiel237@gmail.com>
2023-04-13 18:29:01 -0300

    Add Ejercicio 2 Unidad 3

---
+++ Migrations/20230413211744_AddBirthdateToCustomer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirstApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBirthdateToCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Birthdate",
+                table: "Customer",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthdate",
+                table: "Customer");
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,6 +7,8 @@ namespace FirstApplication.Models
     public int Id { get; set; }
     [StringLength(255)]
     public string Name { get; set; }
+    [StringLength(10)]
+    public string? Birthdate { get; set; }
     public bool IsSubcribedToNewsletter { get; set; }
     public MembershipType MembershipType { get; set; }
     public byte MemberShipTypeId { get; set; }
